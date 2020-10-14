97c01e4e6ea7fc118ed5b890224e7cb3f56c0346
Rupal Jain <rupaljain.1699@gmail.com>
2020-10-14 23:59:16 +0530

    created home & header files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Airbnb clone</h1>
+      <Header />
+      {/* Home */}
+      <Home />
+      {/* Header */}
+      {/* Banner */}
+      {/* Search */}
+      {/* Cards */}
+      {/* Footer */}
+
+      {/* SearchPage */}
     </div>
   );
 }
