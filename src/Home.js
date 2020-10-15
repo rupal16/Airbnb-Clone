5b0359278e1185e2438c3db8f0ae8bb43051b577
Rupal Jain <rupaljain.1699@gmail.com>
2020-10-15 23:34:28 +0530

    completed responsive header component

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,7 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return (
-    <div className="home">
-      <h1>Home</h1>
-    </div>
-  );
+  return <div className="home"></div>;
 }
 
 export default Home;
